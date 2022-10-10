188ed1c22d564628244fba7e9c04f83fb61d8664
Luisibariaz <luisitorey.lip@gmail.com>
2022-10-10 10:26:41 -0500

    Examen

---
+++ ejercicios.js
@@ -0,0 +1,74 @@
+
+// EJERCICIO 1.
+
+// const tipado = "Hello There ..";
+// const numer = 20;
+// console.log(`"El contenido es de tipo: ${typeof(tipado)} Y ${typeof(numer)}`)
+
+
+// EJERCICIO 2.
+
+// const ejemplo = prompt('escribe un texto ..') 
+// const palabra = prompt('Escribe la palabra que buscas')
+
+//  if (ejemplo.includes(palabra))
+//  {
+//     console.log(`Se obtuvo la palabra: ${palabra} ${ejemplo.split(palabra)[1]}`)
+  
+//  }else
+//  {
+//      console.log('No se encontro la palabra, XDD')
+//  }
+
+ // EJERCICIO 4.
+
+// for (i=2;i<50;i=i+2){
+//     console.log(i)
+//  console.log('pares: ')
+// }
+
+// for (i=1;i<50;i=i+2){
+//     console.log(i)
+// console.log('impares: ')
+// }
+
+// OTRA FORMA DE HACERLO..
+
+// const pares = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30]
+// const impares = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29]
+
+// const numeros = prompt("escribe si quieres pares o impares: ")
+
+// if (numeros=="impares"){
+//     console.log(impares)
+// }if(numeros=="pares"){
+//     console.log(pares)
+// } else {
+//     console.log("nada que decir ..")
+// }
+
+// EJERCICIO 6
+
+// const altura = prompt ('Programa para crear un rextangulo. \nPor favor, Escribe la altura:');
+// const ancho = prompt('Ahora ingrese el ancho del rectangulo')
+
+// for(let i=1; i<=altura; ++i){
+//     for (let j=1; j<= ancho; ++j){
+//         console.log("*");
+//     }
+//     console.log("*")
+// }
+
+// EJERCICIO 7
+
+// const altura = prompt ('Programa para crear un triangulo. \nPor favor, Escribe la altura del triangulo que desees:');
+
+// for(let i=1; i<=altura; ++i){
+//     let resultado = '';
+
+//     for (let j = 1; j<=i; ++j){
+//         resultado += '*';
+//     }
+//     console.log(resultado)
+// }
+//
